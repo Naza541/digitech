4c796f6b2259ec2df2c504aae778d4435c3eef39
Naza541 <princeimpact18@gmail.com>
2026-03-11 19:45:40 +0100

    chore

--- index.js
+++ index.js
@@ -1,8 +1,120 @@
-// This is my first JavaScript code, I am learning how to use it to make my website more interactive and dynamic.
-console.log("Welcome to DigiTech");
 
-let name = "DigiTech Hub";
-console.log(name);
+let students = [
+    {
+        firstName : "John",
+        lastName : "Doe",
+        age : "20",
+        location : "New York",
+        country : "USA",
+        IsLocatedInImo : false,
+    },
 
-let founder = "Princewill";
-console.log();
+    {
+        firstName : "Jane",
+        lastName : "Smith",
+        age : "22",
+        location : "Imo",
+        country : "Nigeria",
+        IsLocatedInImo : true,
+    },
+
+    {
+        firstName : "Michael",
+        lastName : "Johnson",
+        age : "19",
+        location : "Lagos",
+        country : "Nigeria",
+        IsLocatedInImo : false,
+    },
+
+    {
+        firstName : "Emily",
+        lastName : "Davis",
+        age : "21",
+        location : "Imo",
+        country : "Nigeria",
+        IsLocatedInImo : true,
+    },
+
+    {
+        firstName : "David",
+        lastName : "Wilson",
+        age : "23",
+        location : "Abuja",
+        country : "Nigeria",
+        IsLocatedInImo : false,
+    },
+
+    {
+        firstName : "Sarah",
+        lastName : "Onyeka",
+        age : "20",     
+        location : "Imo",
+        country : "Nigeria",
+        IsLocatedInImo : true,
+    },
+
+    {
+        firstName : "James",
+        lastName : "Brown",
+        age : "24",
+        location : "Port Harcourt",
+        country : "Nigeria",
+        IsLocatedInImo : false,
+    },
+
+    {
+        firstName : "Olivia",
+        lastName : "Smith",
+        age : "22",
+        location : "Imo",
+        country : "Nigeria",
+        IsLocatedInImo : true,
+    },
+
+    {
+        firstName : "William",
+        lastName : "Johnson",
+        age : "21", 
+        location : "Kano",
+        country : "Nigeria",
+        IsLocatedInImo : false,
+    },
+
+    {
+        firstName : "Ava",
+        lastName : "Davis",
+        age : "20",    
+        location : "Ontario",
+        country : "Canada",
+        IsLocatedInImo : false,
+    }
+];
+
+console.log(students);
+
+
+function NumberSquare(number) {
+return number * number
+}
+
+console.log(NumberSquare(5))
+
+function TrianglePerimeter(value) {
+    return value + value + value
+}
+
+console.log(TrianglePerimeter(17))
+
+function CubeVolume(value) {
+    return value * value * value
+}
+
+console.log(CubeVolume(5))
+
+function WelcomeNewMembers(greeting){
+    return greeting
+}
+
+console.log(WelcomeNewMembers("Welcome to the Family!"))
+
